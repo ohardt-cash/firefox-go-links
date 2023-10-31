00388b030e8a5bf82fa70aacea2d7e90420ecf99
Oliver Hardt <ohardt@squareup.com>
2023-10-30 17:17:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Go Links for Firefox
+
+
+### Install
+You can find the latest version the [releases page](https://github.com/ohardt-cash/firefox-go-links/releases).
+Click the `.xpi` and it should install automatically.
---
+++ background.js
@@ -0,0 +1,47 @@
+browser.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        var goLinkKeyword = "";
+
+        const url = new URL(details.url);
+        /*
+            basic case: http(s)://go/firefox-go-links
+         */
+        if (url.hostname === "go") {
+            goLinkKeyword = url.pathname;
+        } else {
+            /*
+                try catching go links that already got forwarded to Google search
+                this happens when someone enters "go/firefox-go-links" into the searchbar and hits enter
+             */
+            const queryParams = new URLSearchParams(url.search);
+            const queryParamValue = queryParams.get("q");
+            if (url.hostname === "www.google.com" &&
+                url.pathname === "/search" &&
+                queryParamValue && queryParamValue.startsWith("go/")) {
+
+                const decodedQueryParam = decodeURIComponent(queryParams.get("q"));
+                const keywordMatch = decodedQueryParam.match(/go\/(.+)/);
+                if (keywordMatch) {
+                    goLinkKeyword = keywordMatch[1];
+                }
+            }
+
+            // todo: catch more search engines like duckduckgo, etc
+        }
+
+        if (goLinkKeyword.startsWith("/")) {
+            goLinkKeyword = goLinkKeyword.slice(1);
+        }
+
+        if (goLinkKeyword !== "") {
+            const newURL = `https://go.sqprod.co/${goLinkKeyword}`;
+            console.log(`firefox-go-links: redirecting to ${newURL}`);
+            return {redirectUrl: newURL};
+        }
+    },
+    {
+        urls: ["<all_urls>"],
+        types: ["main_frame"],
+    },
+    ["blocking"]
+);
